46fd62b250f1048f5b4d97b21482378eece0f587
Anton <antoki@yandex.ru>
2024-04-30 18:25:42 +0300

    fix: pom.xml

--- stats-module/stats_server/src/main/java/ru/practicum/stat_service/exception/InvalidTimeParamException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.stat_service.exception;
-
-public class InvalidTimeParamException extends RuntimeException{
-    public InvalidTimeParamException(String msg){
-        super(msg);
-    }
-}
