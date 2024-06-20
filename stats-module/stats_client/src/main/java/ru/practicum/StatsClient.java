a0febe6f38933db7451c55c50a18f0bf1a5c0d2b
Anton <antoki@yandex.ru>
2024-06-20 19:41:03 +0300

    fix: checkstyle

--- stats-module/stats_client/src/main/java/ru/practicum/StatsClient.java
+++ stats-module/stats_client/src/main/java/ru/practicum/StatsClient.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.dto.HitDto;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
